7b1b34438795d5177d66d90cc6f815dbd8e7ae41
Alrons <titun766@gmail.com>
2025-04-04 22:03:45 +0500

    Start project

---
+++ LINQ/LINQ/Collections/Lists.cs
@@ -0,0 +1,27 @@
+﻿namespace Collections
+{
+    public class Lists
+{
+    public List<string> stringList { get; set; }
+        
+    public List<int> intList { get; set; }
+
+    public Lists()
+    {
+        stringList = new List<string> {
+        "one",
+        "two",
+        "tree",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eght",
+        "nine",
+        "ten"
+        };
+        intList = new List<int> {1,2,3,4,5,6,7,8,9,10};
+    }
+
+}
+}
---
+++ LINQ/LINQ/LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using Collections;
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lists lists = new Lists();
+
+            ShowDiference("Отфильтровал все числа равные 5",lists.intList, lists.intList.Where(x => x == 5).ToList());
+        }
+
+        private static void ShowDiference(string whatChanged,List<int> firstList, List<int> secondList)
+        {
+            // Общий заголовок
+            Console.WriteLine(whatChanged);
+
+            // Заголовки таблицы
+            Console.WriteLine("{0,-15} {1,-15}", "Изначальный", "Измененный");
+
+            // Вывод данных
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                if(secondList.Count > i)
+                Console.WriteLine("{0,-15} {1,-15}", firstList[i], secondList[i]);
+
+                else Console.WriteLine("{0,-15} {1,-15}", firstList[i], "-");
+            }
+        }
+
+    }
+}
