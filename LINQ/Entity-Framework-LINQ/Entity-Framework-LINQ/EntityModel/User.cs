ca074fb6616e70e208b1a01198ddec90cdcf3d90
Alrons <titun766@gmail.com>
2025-04-08 20:39:45 +0500

    пока что все
    тут не много потому что выполняются все теже запросы из system.Linq. поэтому повторять я не вижу смысла

---
+++ LINQ/Entity-Framework-LINQ/Entity-Framework-LINQ/EntityModel/User.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Entity_Framework_LINQ.EntityModel
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
--- LINQ/Entity-Framework-LINQ/Entity-Framework-LINQ/Program.cs
+++ LINQ/Entity-Framework-LINQ/Entity-Framework-LINQ/Program.cs
@@ -1,10 +1,121 @@
-﻿namespace Entity_Framework_LINQ
+﻿using Entity_Framework_LINQ.EntityModel;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+
+namespace Entity_Framework_LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //User newUser = new User() { Name = "NewUserName2", Age = 24 };
+            //Console.WriteLine("___________________________________________");
+            //AddUser(newUser);
+            //GetAllUsers();
+            //Console.WriteLine("___________________________________________");
+            //GetUsersByFilter(u => u.Age < 25);
+            //Console.WriteLine("___________________________________________");
+            //GetOrderByUsers(u => u.Age);
+            //Console.WriteLine("___________________________________________");
+            //EditUsersPlusAge(newUser);
+            //Console.WriteLine("___________________________________________");
+            //SQLUser("SELECT \"Id\", \"Name\", \"Age\"\r\n\tFROM public.\"User\" WHERE \"Age\" > 20");
+        }
+
+        // получаем список пользователей
+        public static List<User> GetAllUsers ()
+        {
+            List < User > users = new List<User> ();
+            using (var context = new AppDbContext())
+            {
+                users = context.User.ToList();
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name + " Age: " + user.Age);
+                }
+            }
+            return users;
+        }
+
+        // добовляем пользователя
+        public static void AddUser(User user)
+        {
+            using (var context = new AppDbContext())
+            {
+                context.User.Add(user);
+                context.SaveChanges();
+            }
+        }
+
+        // получаем пользователя по булевому условию (например: u => u.Age < 30)
+        public static void GetUsersByFilter (Func<User,bool> func)
+        {
+            using (var context = new AppDbContext())
+            {
+                var users = context.User.Where(func).ToList();
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name + " Age: " + user.Age);
+                }
+            }
+        }
+
+        // выводим отсортированных пользователей
+        public static void GetOrderByUsers<TKey>(Func<User, TKey> func)
+        {
+            using (var context = new AppDbContext())
+            {
+                var users = context.User.OrderBy(func).ToList();
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name + " Age: " + user.Age);
+                }
+            }
+        }
+
+
+        // добавляем пользователю 1 год
+        public static void EditUsersPlusAge(User oneUser)
+        {
+            using (var context = new AppDbContext())
+            {
+                var user = context.User.FirstOrDefault(u => u.Name == oneUser.Name);
+                if (user != null)
+                {
+                    user.Age++;
+                    context.SaveChanges();
+                }
+                else Console.WriteLine("UserNotFound");
+            }
+        }
+
+        // удаляем польователя
+        public static void DeleteUser (User oneUser)
+        {
+            using (var context = new AppDbContext())
+            {
+                var user = context.User.FirstOrDefault(u => u.Name == oneUser.Name);
+                if (user != null)
+                {
+                    context.User.Remove(user);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        // получаем пользователя через SQL
+        public static void SQLUser(string query)
+        {
+            using (var context = new AppDbContext())
+            {
+                var users = context.User.FromSqlRaw(query).ToList();
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name + " Age: " + user.Age);
+                }
+            }
         }
     }
 }
