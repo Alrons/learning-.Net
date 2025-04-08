f8f53407ae699a1d5068032daf286fa5758abd41
Alrons <titun766@gmail.com>
2025-04-08 20:40:08 +0500

    Squashed commit of the following:
    commit ca074fb6616e70e208b1a01198ddec90cdcf3d90
    Author: Alrons <titun766@gmail.com>
    Date:   Tue Apr 8 20:39:45 2025 +0500
    
        пока что все
    
        тут не много потому что выполняются все теже запросы из system.Linq. поэтому повторять я не вижу смысла
    
    commit a5b874f54d577d0f849d763cd2d1a2a63b750abe
    Author: Alrons <titun766@gmail.com>
    Date:   Tue Apr 8 19:11:56 2025 +0500
    
        start

---
+++ LINQ/Entity-Framework-LINQ/Entity-Framework-LINQ/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Entity_Framework_LINQ.EntityModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity_Framework_LINQ
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=testBd;Username=postgres;Password=root");
+        }
+    }
+}
---
+++ LINQ/Entity-Framework-LINQ/Entity-Framework-LINQ/EntityModel/Product.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Entity_Framework_LINQ.EntityModel
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
