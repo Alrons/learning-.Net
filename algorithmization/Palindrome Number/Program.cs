717187aaec03c4a0ff5f88b64fe923491335792d
Alrons <115451394+Alrons@users.noreply.github.com>
2025-04-13 15:57:46 +0500

    start branch

---
+++ algorithmization/Palindrome Number/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // my code beats 79% | 82% || runtime || memory
+        public bool IsPalindrome(int x)
+        {
+            int original = x;
+            int reversed = 0;
+
+            while (x > 0)
+            {
+                int digit = x % 10;
+                reversed = reversed * 10 + digit;
+                x /= 10;
+            }
+
+            return original == reversed;
+
+        }
+
+        // exeple 99.9, 94.04
+        // sometime get 79 runtime
+        public bool IsPalindromeEX(int x)
+        {
+            int r = 0, c = x;
+            while (c > 0)
+            {
+                r = r * 10 + c % 10;
+                c /= 10;
+            }
+            return r == x;
+        }
+    }
+}
---
+++ algorithmization/Two Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Two_Sum
+{
+    // все примеры и условия задачи с одноименным названием (проекта) на leetcode
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TwoSum([2, 7, 11, 15], 9)); // => [0,1]
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numMap = new Dictionary<int, int>(); // A mapping to store numbers and their indices
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i]; // Find the required number to reach the target
+                if (numMap.ContainsKey(complement))
+                {
+                    return new int[] { numMap[complement], i }; // Return indices of the complement and current number
+                }
+                numMap[nums[i]] = i; // Store the number with its index
+            }
+            return new int[] { }; // This line is never reached due to the problem guarantee
+        }
+    }
+}
